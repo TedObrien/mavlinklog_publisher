59aaad1533a2fd6c04d1da5fcab327a8c59c4a83
TedObrien <tedobr1998@gmail.com>
2024-10-06 21:33:33 +0100

    improved command line args

--- src/mavlinklog_publisher_node.cpp
+++ src/mavlinklog_publisher_node.cpp
@@ -2,18 +2,15 @@
 #include <functional>
 #include <memory>
 #include <string>
-
 #include "rclcpp/rclcpp.hpp"
 #include "mavlinklog_publisher/msg/mavlink_log_msg.hpp"
 #include "px4_msgs/msg/mavlink_log.hpp"
 
-// Script needs to subscribe to the MavlinkLogMsg and publish to the MavlinkLog message
-
-// add command line arg for starting message
-// add command line arg for topic prefix
-
-
+using namespace std::chrono_literals;
 using std::placeholders::_1;
+
+// TODO
+// Best practise on variable,class names ect
 
 class MinimalSubscriber : public rclcpp::Node
 {
@@ -21,11 +18,22 @@ public:
   MinimalSubscriber()
   : Node("mavlinklog_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<px4_msgs::msg::MavlinkLog>("/fmu/in/mavlink_log", 10);
-    // timer_ = this->create_wall_timer(
-    //   2s, std::bind(&MinimalSubscriber::timer_callback, this));
+    this->declare_parameter("uav_name", ""); 
+    this->declare_parameter("message_on_start", true);
+
+    ran_ = false;
+
+    px4_publisher_ = this->create_publisher<px4_msgs::msg::MavlinkLog>(
+      this->get_parameter("uav_name").as_string() + "/fmu/in/mavlink_log", 10);
+
+    log_publisher_ = this->create_publisher<mavlinklog_publisher::msg::MavlinkLogMsg>(
+      this->get_parameter("uav_name").as_string() + "mavlink_log_msg", 10);
+
+    timer_ = this->create_wall_timer(
+      5s, std::bind(&MinimalSubscriber::timer_callback, this));
+
     subscription_ = this->create_subscription<mavlinklog_publisher::msg::MavlinkLogMsg>(
-    "mavlink_log_msg", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      "mavlink_log_msg", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     
   }
   
@@ -34,10 +42,7 @@ private:
   void topic_callback(const mavlinklog_publisher::msg::MavlinkLogMsg & msg)
   {
 
-    // When message received, Publish to mavlink_log 
     auto message = px4_msgs::msg::MavlinkLog();
-    message.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    message.severity = msg.level;
 
     // Convert revieved string to ascii struct
     std::string received_text = msg.message;
@@ -65,29 +70,38 @@ private:
     // Ensure the last element is null-terminated
     message.text[std::min(received_text.size(), message.text.size() - 1)] = '\0';
 
+
+    // Populate mavlink_log message
+    message.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    message.severity = msg.level;
+
     RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << received_text << "' with a severity of:" << unsigned(msg.level));
 
-    publisher_->publish(message); 
+    px4_publisher_->publish(message); 
     
   }
   
-  // Correct type for publisher_ member variable
-  rclcpp::Publisher<px4_msgs::msg::MavlinkLog>::SharedPtr publisher_;
-  // rclcpp::TimerBase::SharedPtr timer_;
-  size_t count_;
-
+  
   void timer_callback()
   {
-    auto message = mavlinklog_publisher::msg::MavlinkLogMsg();
-    message.level = 5;
-    message.id = "test_publisher";
-    message.message = "Hello World";
-    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.message << "'");
-    // publisher_->publish(message);
+
+    if (!ran_ &&  this->get_parameter("message_on_start").as_bool()){
+
+      auto message = mavlinklog_publisher::msg::MavlinkLogMsg();
+      message.level = 5;
+      message.message = "MavlinkLog Publisher started ";
+      log_publisher_->publish(message);
+      ran_ = true;
+    }
+    
   }
-
+  
+  bool ran_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<px4_msgs::msg::MavlinkLog>::SharedPtr px4_publisher_;
+  rclcpp::Publisher<mavlinklog_publisher::msg::MavlinkLogMsg>::SharedPtr log_publisher_;
   rclcpp::Subscription<mavlinklog_publisher::msg::MavlinkLogMsg>::SharedPtr subscription_;
-
+  size_t count_;
 
 
 };
